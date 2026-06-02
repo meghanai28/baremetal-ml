cdd2d45897c245e3da7e297208dc0902534e9dd7
meghanai28 <meghana.indukuri@gmail.com>
2026-06-02 16:15:15 -0700

    cleaner

--- linreg.c
+++ linreg.c
@@ -22,6 +22,50 @@ float predict(const float x[D], const float w[D], float b)
     return pred;
 }
 
+float compute_loss_and_gradients(const float X[N][D], const float y[N], const float w[D], float b, float grad_w[D], float *grad_b)
+{
+
+    for (int j = 0; j < D; j++)
+    {
+        grad_w[j] = 0.0f;
+    }
+    *grad_b = 0.0f;
+    float total_loss = 0.0f;
+
+    for (int i = 0; i < N; i++)
+    {
+        float pred = predict(X[i],w,b);
+        float r_error = pred - y[i];
+
+        for (int j = 0; j<D; j++)
+        {
+            grad_w[j] += r_error * X[i][j];
+        }
+
+        total_loss += r_error * r_error;
+        *grad_b += r_error;
+    }
+
+    for (int i = 0; i <D; i++)
+    {
+        grad_w[i] = (2.0f/N) * grad_w[i];
+    }
+        
+    *grad_b = (2.0f/N) * (*grad_b);
+        
+    float mse = total_loss/N;
+    return mse;
+}
+
+void update_params(float w[D], float *b, const float grad_w[D], float grad_b)
+{
+    for (int i =0; i<D; i++)
+    {
+        w[i] -= LR * grad_w[i];
+    }
+    *b -= LR * grad_b;
+}
+
 int main (void)
 {
     srand(0);
@@ -30,7 +74,6 @@ int main (void)
 
     // X - features/inputs
     // y - the true label
-
     float X[N][D];
     float y[N];
 
@@ -46,64 +89,22 @@ int main (void)
         y[i] = 3.0f * X[i][0] - 2.0f * X[i][1] + 0.5f * X[i][2] + 4.0f;
     }
 
-    // print the dataset
-    // for (int i = 0; i<N; i++)
-    // {
-    //     printf("row %d: ", i);
-    //     for (int j = 0; j< D; j++)
-    //     {
-    //         printf("X[%d][%d] = %.3f ", i,j, X[i][j]);
-    //     }
-    //     printf("y = %.3f\n", y[i]);
-    // }
-
     // initalize weights and bias
     float w[D] = {0.0f};
     float b = 0.0f;
 
-    // printf("\n inital model params \n");
-
-    // for (int i = 0; i<D; i ++)
-    // {
-    //     printf("w[%d] = %.3f\n", i, w[i]);
-    // }
-    // printf("b = %.3f\n", b);
-
-
     for (int e = 0; e < EPOCHS; e ++)
     {
-        float grad_w[D] = {0.0f};
-        float grad_b = 0.0f;
-        float total_loss = 0.0f;
-
-        for (int i = 0; i < N; i++)
-        {
-            float pred = predict(X[i],w,b);
-            float r_error = pred - y[i];
-
-            for (int j = 0; j<D; j++)
-            {
-                grad_w[j] += r_error * X[i][j];
-            }
-
-            total_loss += r_error * r_error;
-            grad_b += r_error;
-        }
-
-        for (int i = 0; i <D; i++)
-        {
-            grad_w[i] = (2.0f/N) * grad_w[i];
-            w[i] -= LR * grad_w[i];
-        }
         
-        grad_b = (2.0f/N) * grad_b;
-        b -= LR * grad_b;
-        
-        float mse = total_loss/N;
+        float grad_w[D];
+        float grad_b;
+
+        float mse = compute_loss_and_gradients(X,y,w,b,grad_w,&grad_b);
+        update_params(w,&b,grad_w,grad_b);
 
         if (e % 100 == 0)
         {
-            printf("epoch = %d & MSE = %.3f\n", e, mse);
+            printf("epoch = %d & MSE = %.8f\n", e, mse);
         }
     }
 
